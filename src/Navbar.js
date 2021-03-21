f4ec5b276c22ee875ef9e97bbd9a5844bffa075a
Gabriel Rezende <g@brielrezen.de>
2021-03-21 00:34:00 -0300

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>Catalogo</h1>
+      <div className="links">
+        <a href="/filmes">Vídeo</a>
+        <a href="/filmes">Audio</a>
+        <a href="/filmes">Livro</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;
